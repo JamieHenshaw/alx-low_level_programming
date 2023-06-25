b6a1d9cb87089f029db3e9b86d3dbbc461ab6583
JamieHenshaw <jlhworklife@gmail.com>
2023-06-25 08:23:57 -0700

    task0x0A edit008

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints its name, followed by a new line
- * @argc: Number of command line arguments
- * @argv: Array that contains the programme command line arguments
- *
- * Return: 0 (success)
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv[0]);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
